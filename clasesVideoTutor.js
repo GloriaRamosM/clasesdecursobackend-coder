841fe1b952982030b59f0eec5b7cef3262f2f87b
GloriaRamosM <gloriaramosmoran@gmail.com>
2024-01-26 17:06:54 -0300

    ejemplo de classe y clases anidada usando constructor

--- clasesVideoTutor.js
+++ clasesVideoTutor.js
@@ -19,10 +19,27 @@ class Animal {
   }
 }
 
+class Perro extends Animal {
+  constructor(nombre, genero, tamanio) {
+    //super() es un metodo que manda a llamar al constructor de la clase padre
+    //( Animal en este caso) que estoy usando en Perro
+    super(nombre, genero);
+    this.tamanio = tamanio;
+  }
+
+  sonar() {
+    console.log(" Soy un perro y mi sonido es un ladrido");
+  }
+
+  ladrar() {
+    console.log("yo digo Guaauu guauu");
+  }
+}
+
 /* para llamar a los metodos de una clase, debo llamar a la variable a la cual 
 le asigne la clase, y luego ingresar a ese metodo que seria una funcion en realidad*/
 
-const perrito = new Animal("poshi", "macho");
+const perrito = new Perro("poshi", "macho", "mediano");
 const perrita = new Animal("cofi", "hembra");
 console.log(perrita);
 perrita.saludar();
@@ -30,3 +47,4 @@ perrita.sonar();
 console.log(perrito);
 perrito.saludar();
 perrito.sonar();
+perrito.ladrar();
