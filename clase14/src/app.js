fc671e7edb684a63f6c2c3ecaab3cedaaaef67f8
GloriaRamosM <gloriaramosmoran@gmail.com>
2024-03-19 13:17:01 -0300

    agregue el archivo env para ocultar mis variables de entorno

--- clase14/src/app.js
+++ clase14/src/app.js
@@ -1,14 +1,16 @@
 import express from "express";
 import mongoose from "mongoose";
 import studentsRouter from "./routes/studentsRouter.js";
+import dotenv from "dotenv";
 
+// variables de entorno
+dotenv.config();
 const app = express();
 const PORT = 8080;
 const server = app.listen(PORT, () => console.log("Server running in ", PORT));
 
-const DB_URL =
-  "mongodb+srv://gloriaramosmoran:gloria7579rm23@cluster0.tthdxw1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-const connection = mongoose.connect(DB_URL);
+console.log("DB_URL:", process.env.DB_URL);
+const connection = mongoose.connect(process.env.DB_URL);
 
 app.use(express.json());
 app.use(studentsRouter);
