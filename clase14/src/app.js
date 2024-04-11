6be93b0e0c373343e0e5aa7a06fce1a4a48e15f7
GloriaRamosM <gloriaramosmoran@gmail.com>
2024-04-10 22:08:17 -0300

    practicas

--- clase14/src/app.js
+++ clase14/src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import studentsRouter from "./routes/studentsRouter.js";
 import dotenv from "dotenv";
+import session from "express-session";
 
 // variables de entorno
 dotenv.config();
@@ -10,7 +11,15 @@ const PORT = 8080;
 const server = app.listen(PORT, () => console.log("Server running in ", PORT));
 
 console.log("DB_URL:", process.env.DB_URL);
-const connection = mongoose.connect(process.env.DB_URL);
+const connect = mongoose.connect(process.env.DB_URL);
 
 app.use(express.json());
 app.use(studentsRouter);
+
+app.use(
+  session({
+    secret: "Secreto",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
