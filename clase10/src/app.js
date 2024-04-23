a53f35d7b3bae396d0405599d7a498537ecfb04a
GloriaRamosM <gloriaramosmoran@gmail.com>
2024-04-23 17:06:17 -0300

    JWT PRIMERA PRACTICA

--- clase10/src/app.js
+++ clase10/src/app.js
@@ -1,41 +1,77 @@
 import express from "express";
-import __dirname from "./utils.js";
 import handlebars from "express-handlebars";
-import { Server } from "socket.io";
-import viewRouter from "./routes/views.router.js";
-
+import { authToken, generateToken } from "./utils.js";
 const app = express();
-const port = 8080;
 
-//Middlewares
-app.set("views", __dirname + "/views");
-app.set("view engine", "handlebars");
-app.use(express.urlencoded({ extended: true }));
+//middlewares
 app.use(express.json());
-app.use(express.static(__dirname + "/public"));
+
+//settings
 app.engine("handlebars", handlebars.engine());
+app.set("views", "./src/views");
+app.set("view engine", "handlebars");
 
-//app.use(viewRouter);
+app.use(express.static("./src/public"));
 
+// usuarios en memoria DB
+const users = [];
+
+//view routes
 app.get("/", (req, res) => {
-  res.render("home", { logs });
+  res.render("home");
 });
 
-const server = app.listen(port, () => console.log("Listening in", port));
-
-const io = new Server(server); // instanciando socket.io
-
-const logs = [];
-
-io.on("connection", (socket) => {
-  console.log("Connected!");
-
-  socket.on("message", (data) => {
-    console.log(data);
-
-    logs.push({ socketid: socket.id, message: data });
-
-    io.emit("log", { logs });
-    console.log(logs);
-  });
+app.get("/register", (req, res) => {
+  res.render("register");
 });
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+//API ROUTES
+// REGISTER
+app.post("/api/register", (req, res) => {
+  const { name, email, password } = req.body;
+  const exists = users.find((user) => user.email === email);
+  if (exists)
+    return res
+      .status(400)
+      .send({ status: "error", error: "User already exists" });
+
+  //Creamos el usuario
+  const user = {
+    name,
+    email,
+    password,
+  };
+
+  // SI TENEMOS UN NUEVO USUARIO SE LO PASAMOS POR PARAMETRO A LA FUNCION GENERATE-TOKEN aca es logica de token
+  users.push(user);
+  const access_token = generateToken(user);
+  res.send({ status: "success", access_token });
+});
+
+//LOGIN
+app.post("/api/login", (req, res) => {
+  const { email, password } = req.body;
+  const user = users.find(
+    (user) => user.email === email && user.password === password
+  );
+  console.log(user);
+  if (!user)
+    return res
+      .status(400)
+      .send({ status: "error", error: "Invalid credentials" });
+
+  // aca va la logica de token
+  const access_token = generateToken(user);
+  res.send({ status: "success", access_token });
+});
+
+app.get("/api/current", authToken, (req, res) => {
+  res.send({ status: "success", payload: req.user });
+});
+
+//Listeners
+app.listen(5000, () => console.log("Listening on 5000"));
--- clase10/src/utils.js
+++ clase10/src/utils.js
@@ -1,7 +1,28 @@
-import { fileURLToPath } from "url";
-import { dirname } from "path";
+import jwt from "jsonwebtoken";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const PRIVATE_KEY = "CoderKeyFeliz";
 
-export default __dirname;
+export const generateToken = (user) => {
+  //lógica para generar el token
+  const token = jwt.sign({ user }, PRIVATE_KEY, { expiresIn: "1h" });
+  return token;
+};
+
+export const authToken = (req, res, next) => {
+  //lógica de autorización
+  const authHeader = req.headers.authorization;
+  if (!authHeader)
+    return res.status(401).send({ status: "error", message: "No autorizado" });
+  console.log(authHeader);
+  const token = authHeader.split(" ")[1];
+
+  jwt.verify(token, PRIVATE_KEY, (error, credentials) => {
+    console.log(error);
+    if (error)
+      return res
+        .status(401)
+        .send({ status: "error", message: "No autorizado" });
+    req.user = credentials.user;
+    next();
+  });
+};
